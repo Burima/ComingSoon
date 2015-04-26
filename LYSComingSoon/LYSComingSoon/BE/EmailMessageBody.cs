99cedbb23b2215e5b4192bb9fbb1998f75a8c2df
Goyesh <goyesh.miah@gmail.com>
2015-04-26 12:24:34 +0530

    Initial Commit

---
+++ LYSComingSoon/LYSComingSoon/BE/EmailMessageBody.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LYSComingSoon.BE;
+
+namespace LYSComingSoon.BE
+{
+    public class EmailMessageBody
+    {
+        public string CreateBody(User user, string messagebodyPath)
+        {
+            string message = System.IO.File.ReadAllText(messagebodyPath);
+            message = message.Replace("#first_name", user.first_name);
+            message = message.Replace("#last_name", user.last_name);
+            message = message.Replace("#contact_email", user.contact_email);
+            message = message.Replace("#contact_subject", user.contact_subject);
+            message = message.Replace("#message", user.message);           
+
+            return message.ToString();
+        }
+    }
+}
---
+++ LYSComingSoon/LYSComingSoon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LYSComingSoon.MvcApplication" Language="C#" %>
